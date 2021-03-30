785c83a2e527bab44e57706b57fca2462a62d0ab
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-03-30 16:33:45 -0700

    create test for get route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,4 +1,12 @@
 const request = require("supertest");
+const fs = require("fs");
 const app = require("../lib/app");
 
-describe("static server get route", () => {});
+describe("static server get route", () => {
+  it('should return a response with the contents of "dancin.txt" as the body', async () => {
+    const response = await request(app).get("localhost:1111/dancin.txt");
+    const actual = await fs.readFile("../public/dancin.txt");
+
+    expect(response.text).toEqual(actual);
+  });
+});
