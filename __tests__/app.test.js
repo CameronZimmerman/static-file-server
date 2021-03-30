2502a778034d3a0a69caf5b56628d483f27c5dda
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-03-30 09:17:26 -0700

    create test for /echo post route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -27,7 +27,7 @@ describe("app routes", () => {
 
   it('should return html with an h1 and the word blue by requesting "/blue" ', async () => {
     try {
-      const data = await request(app).get("/red");
+      const data = await request(app).get("/blue");
       console.log(data.text);
       expect(data.text).toEqual(`<html>
   <body>
@@ -48,6 +48,16 @@ describe("app routes", () => {
     <h1>Green</h1>
   </body>
 </html>`);
+    } catch (e) {
+      console.log(e.message);
+    }
+  });
+
+  it("should return a 200 status and the content of the request body", async () => {
+    try {
+      const data = await request(app).post("/echo").send("hello");
+
+      expect(data.text).toEqual("hello");
     } catch (e) {
       console.log(e.message);
     }
