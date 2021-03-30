50a9f03d09feed12f092e5fb5d734502d90e1da3
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-03-29 17:18:09 -0700

    create / route test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,9 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('app routes', () => {
-
+  it('should return hi when get requesting "/" ', async() => {
+    const data = await request(app)
+      .get('/');
+    expect(data.body).toEqual('hi');
+  });
 });
